231156330edcdbb7fa5c955fb2c9a10978fb191d
Thullyoo <thullyofilho@gmail.com>
2025-09-16 14:46:02 -0300

    Initial commit

---
+++ src/main/java/br/thullyoo/desafio_backend_fcamara/DesafioBackendFcamaraApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.desafio_backend_fcamara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendFcamaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendFcamaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyoo/desafio_backend_fcamara/DesafioBackendFcamaraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.desafio_backend_fcamara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendFcamaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
