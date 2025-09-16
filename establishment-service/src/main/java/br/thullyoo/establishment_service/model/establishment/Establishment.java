6bf358b3ee4062dd791a5f076c7e6f71ac2d6160
Thullyoo <thullyofilho@gmail.com>
2025-09-16 15:18:33 -0300

    feat: add establishment entity

--- establishment-service/src/main/java/br/thullyoo/establishment_service/model/establishment/Establishment.java
+++ establishment-service/src/main/java/br/thullyoo/establishment_service/model/establishment/Establishment.java
@@ -1,5 +1,40 @@
 package br.thullyoo.establishment_service.model.establishment;
 
-@Table
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_ESTABLISHMENT")
+@RequiredArgsConstructor
+@Data
 public class Establishment {
+
+    @Id
+    @JsonProperty(namespace = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @JsonProperty(namespace = "name")
+    @Column(name = "name")
+    private String name;
+
+    @JsonProperty(namespace = "address")
+    @Column(name = "address")
+    private String address;
+
+    @JsonProperty(namespace = "phoneNumber")
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
+
+    @JsonProperty(namespace = "motorcycleParkingSpaces")
+    @Column(name = "motorcycle_parking_spaces")
+    private String motorcycleParkingSpaces;
+
+    @JsonProperty(namespace = "carParkingSpaces")
+    @Column(name = "car_parking_spaces")
+    private String carParkingSpaces;
 }
