10ff894965384dbd4763f4a7c56d1e34b9af27dd
Thullyoo <thullyofilho@gmail.com>
2025-09-17 12:51:44 -0300

    feat: add vehicle model

---
+++ vehicles-service/src/main/java/br/thullyoo/vehicles_service/model/vehicle/Vehicle.java
@@ -0,0 +1,37 @@
+package br.thullyoo.vehicles_service.model.vehicle;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_VEHICLE")
+@Data
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.UUID)
+    public UUID id;
+
+    @JsonProperty(namespace = "model")
+    @Column(name = "model")
+    public String model;
+
+    @JsonProperty(namespace = "mark")
+    @Column(name = "mark")
+    public String mark;
+
+    @JsonProperty(namespace = "color")
+    @Column(name = "color")
+    public String color;
+
+    @JsonProperty(namespace = "type")
+    @Column(name = "type")
+    public VehicleType type;
+
+    @JsonProperty(namespace = "licensePlate")
+    @Column(name = "license_plate", unique = true)
+    public String licensePlate;
+}
