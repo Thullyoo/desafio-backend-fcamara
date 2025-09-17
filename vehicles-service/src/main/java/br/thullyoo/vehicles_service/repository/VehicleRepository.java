387924ddfeb2e4c9f60a84d485ac65c2127a847b
Thullyoo <thullyofilho@gmail.com>
2025-09-17 12:52:43 -0300

    feat: add vehicle repository

---
+++ vehicles-service/src/main/java/br/thullyoo/vehicles_service/repository/VehicleRepository.java
@@ -0,0 +1,11 @@
+package br.thullyoo.vehicles_service.repository;
+
+import br.thullyoo.vehicles_service.model.vehicle.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
+}
