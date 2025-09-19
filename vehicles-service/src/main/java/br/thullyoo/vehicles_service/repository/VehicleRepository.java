b4159d8afef21a782d223e610d4cc39ab5837f10
Thullyoo <thullyofilho@gmail.com>
2025-09-19 14:26:32 -0300

    feat: add new method to get vehicles by establishmentId

--- vehicles-service/src/main/java/br/thullyoo/vehicles_service/repository/VehicleRepository.java
+++ vehicles-service/src/main/java/br/thullyoo/vehicles_service/repository/VehicleRepository.java
@@ -4,8 +4,10 @@ import br.thullyoo.vehicles_service.model.vehicle.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
+    List<Vehicle> findByEstablishmentId(UUID establishmentId);
 }
