564f4601179a0f6595cf00716399105cfbe77111
thullyo <thullyofilho@gmail.com>
2025-09-25 16:10:17 -0300

    feat: add findByLicensePlate on VehicleRepository

--- vehicles-service/src/main/java/br/thullyoo/vehicles_service/repository/VehicleRepository.java
+++ vehicles-service/src/main/java/br/thullyoo/vehicles_service/repository/VehicleRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
     List<Vehicle> findByEstablishmentId(UUID establishmentId);
+    Optional<Vehicle> findByLicensePlate(String licensePlate);
 }
