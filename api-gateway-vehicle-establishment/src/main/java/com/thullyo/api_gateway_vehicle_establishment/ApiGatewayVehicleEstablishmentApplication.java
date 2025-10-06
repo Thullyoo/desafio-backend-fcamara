bc8acb97c3c9071a687326680c8ffe846639c858
thullyo <thullyofilho@gmail.com>
2025-10-06 13:01:42 -0300

    feat: add api gateway project

---
+++ api-gateway-vehicle-establishment/src/main/java/com/thullyo/api_gateway_vehicle_establishment/ApiGatewayVehicleEstablishmentApplication.java
@@ -0,0 +1,13 @@
+package com.thullyo.api_gateway_vehicle_establishment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayVehicleEstablishmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayVehicleEstablishmentApplication.class, args);
+	}
+
+}
---
+++ api-gateway-vehicle-establishment/src/test/java/com/thullyo/api_gateway_vehicle_establishment/ApiGatewayVehicleEstablishmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thullyo.api_gateway_vehicle_establishment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayVehicleEstablishmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
